2e3e24d912093ed85817dc797f932ab58399722b
aavetis <albert.avetisian@gmail.com>
2023-04-17 16:49:24 -0400

    add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from "next/app";
 import { SessionContextProvider } from "@supabase/auth-helpers-react";
 import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { useEffect, useState } from "react";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   const [supabaseClient] = useState(() => createBrowserSupabaseClient());
@@ -10,6 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <div>
       <SessionContextProvider supabaseClient={supabaseClient}>
         <Component {...pageProps} />
+        <Analytics />
       </SessionContextProvider>
     </div>
   );
